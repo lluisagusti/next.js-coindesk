968012fa1e6bd92a223b189e05bdecf8b44fb188
lluisagusti <lluisagustisan@gmail.com>
2020-03-21 20:23:34 +0100

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About this App</h1>
+      <p>Bitcoin prices fom API</p>
+    </div>
+  </Layout>
+);
+
+export default About;
